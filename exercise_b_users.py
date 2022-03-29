9e37332e206b60e0bacb34a39f64edba99f8d79c
ElinVS <elinvsvennberg@gmail.com>
2022-03-29 17:27:48 +0100

    code to return a list of all even numbers

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -71,6 +71,14 @@ print(users["Erik"]["lottery_numbers"][2])
 
 # 6. Return an list of Avril's lottery numbers that are even
 
+avrils_lottery_numbers = users["Avril"]["lottery_numbers"]
+even_number = []
+
+for number in avrils_lottery_numbers:
+  if number % 2 == 0:
+    even_number.append(number)
+
+print(even_number)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
